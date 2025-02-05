260e5d8c5f1d8f2198611c621650040e793d5010
Diana-Raquel <00096520@uca.edu.sv>
2025-02-04 22:15:45 -0600

    feat: url page

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['*/admin/*', '*/api/*'],
     },
-    sitemap: 'https://nextjs.org/sitemap.xml',
+    sitemap: 'https://cruzcruzx2-portafolio.vercel.app/sitemap.xml',
   };
 }
